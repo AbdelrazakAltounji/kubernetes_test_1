18e36c74f23dfb1004ee951ac0f62031e2e2ae8c
Victor <victor.moin1@ibm.com>
2021-03-25 18:25:53 +0100

    edit timeout

--- tests/test.setup.js
+++ tests/test.setup.js
@@ -1,6 +1,6 @@
 const { JasmineAllureReporter } = require('allure-jasmine')
 const { AllureRuntime } = require('allure-js-commons')
-const TIMEOUT = 120000
+const TIMEOUT = 360000
 
 const reporter = new JasmineAllureReporter(
     new AllureRuntime({
