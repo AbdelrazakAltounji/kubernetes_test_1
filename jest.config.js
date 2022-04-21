b12414008cd3230694b14edea57262b317494028
AbdelrazakAltounji <Abdelrazak.alt@hotmail.com>
2022-04-21 13:02:01 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -123,7 +123,7 @@ module.exports = {
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
-    testRunner: "jest-jasmine2"
+    testRunner: "jest-jasmine2",
     // The paths to modules that run some code to configure or set up the testing environment before each test
     // setupFiles: [],
 
